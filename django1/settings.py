3c84feda5cd7573bbbd75a1d7449d4b8c1a5cf2a
Carlos Daniel <danepinheiro17@hotmail.com>
2023-08-29 18:00:25 -0300

    Atualização 29/08/2023

--- django1/settings.py
+++ django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i25m-pvp1ikyulx*38p9progy2kvicpg@1wl4z!i7d9&@@8tmr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
